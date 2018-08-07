3b70afe30f9393e4462fa59de48891a4953a6eed
Daniel Etter <etter.dt@gmail.com>
2018-08-07 15:28:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,12 +22,12 @@ class Song
     end
   end
   
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+  # def artist_name=(name)
+  #   if (self.artist.nil?)
+  #     self.artist = Artist.new(name)
+  #   else
+  #     self.artist.name = name
+  #   end
+  # end
   
 end
