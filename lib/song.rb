1f91ff0b55888eda13c70df6f1f35e5674b6400c
Daniel Etter <etter.dt@gmail.com>
2018-08-07 15:31:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     song = self.new(name)
     artist_name = filename.split(" - ")[0]
     artist = Artist.new(artist_name)
-    Artist.all << artist
+    Artist.all.uniq << artist
     #binding.pry
     
     # if (Artist.all.include?(artist_name))
