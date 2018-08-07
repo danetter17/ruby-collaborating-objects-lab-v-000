36d8cfd338ee58aa9b22aeee23db13663159574d
Daniel Etter <etter.dt@gmail.com>
2018-08-07 15:14:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,16 +9,17 @@ class Song
   def self.new_by_filename(filename)
     name = filename.split(" - ")[1]
     song = self.new(name)
-    #how to remove .mp3?
+    artist_name = filename.split(" - ")[0]
+    artist = Artist.new(artist_name)
     song
   end
   
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+  # def artist_name=(name)
+  #   if (self.artist.nil?)
+  #     self.artist = Artist.new(name)
+  #   else
+  #     self.artist.name = name
+  #   end
+  # end
   
 end
