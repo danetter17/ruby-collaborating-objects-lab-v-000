6819e902a7c7a7e7496664b34a2eb747baf53123
Daniel Etter <etter.dt@gmail.com>
2018-08-07 14:14:25 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require "pry"
+class MP3Importer
+  
+  def initialize(path)
+    @path = path
+  end 
+  
+  def files
+    #where the path goes perhaps?
+    @files = Dir.glob("#{path}/*.mp3")
+  end
+
+  def import(filenames)
+    filenames.each{|filename| Song.new_by_filename(filename)}
+  end
+  
+end
